a3d9c3bd736d6840559e5f8251915d763f784978
logonrm <logonrm@fiap.com.br>
2017-05-03 18:38:24 -0300

    Finishing it

---
+++ www/js/livraria.js
@@ -0,0 +1,27 @@
+﻿$('.collection-item').on('click', function () {
+    var $badge = $('.badge', this),
+        count = $(this).find("span");
+
+    if ($badge.length == 0 || $badge.text() == "") {
+        $badge = $('<span class="badge brown-text">0</span>')
+        .appendTo(this);
+    }
+
+    $badge.text(parseInt($badge.text()) + 1);
+});$('.modal-trigger').leanModal();$('#confirmar').on('click', function () {
+    var texto = '';
+    $('.badge').parent().each(function () {
+        var produto = this.firstChild.textContent;
+        var quantidade = this.lastChild.textContent;
+        texto += produto + ': ' + quantidade + ', ';
+    });
+    $('#resumo').text(texto);
+});
+
+$('.collection').on('click', '.badge', function(){
+    $(this).remove();
+    return flse;
+});$('.acao-limpar').on('click', function () {
+    $('#numero-vendedor').val('');
+    $('.badge').remove();
+});
